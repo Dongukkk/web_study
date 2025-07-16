710d36f1846ed2554730822b7d9c8407c4c3a3e0
Dongukkk <kdu0816@gmail.com>
2025-07-16 10:06:51 +0900

    연습10 scope 개념 활용

---
+++ src/main/java/com/app/controller/study/practice/practice10/Practice10Controller.java
@@ -0,0 +1,34 @@
+package com.app.controller.study.practice.practice10;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Practice10Controller {
+
+	@GetMapping("/practice10/A")
+	public String a() {
+		return "practice/practice10/a";
+	}
+	
+	
+	@GetMapping("/practice10/B")
+	public String b(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		// request / session / application
+		
+		//request : 각 요청별 독립
+		//session : 브라우저(사용자) 별로 구분 사용
+		
+		session.setAttribute("fromBMsg", "fromB");
+		
+		return "practice/practice10/b";
+		
+				
+	}
+	
+	
+}
