781a2e518ca7df7b2ca7ab863e7fcffecd759adf
Dongukkk <kdu0816@gmail.com>
2025-07-25 14:29:36 +0900

    연습16 REST API 응답 처리

---
+++ src/main/java/com/app/controller/study/practice/practice16/Practice16Controller.java
@@ -0,0 +1,35 @@
+package com.app.controller.study.practice.practice16;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.app.dto.user.User;
+
+@Controller
+public class Practice16Controller {
+	
+	@GetMapping("/practice16/quiz1")
+	public String quiz1() {
+		return "practice/practice16/quiz1";
+	}
+	
+	@ResponseBody
+	@GetMapping("/practice16/quiz2")
+	public String quiz2() {
+		return "return rest quiz2";
+	}
+	
+	@ResponseBody
+	@GetMapping("/practice16/quiz3")
+	public User quiz3() {
+		
+		User user = new User();
+		user.setId("quizid");
+		user.setPw("secret");
+		user.setName("quiz이름");
+		user.setUserType("CUS");
+		
+		return user;
+	}
+}
